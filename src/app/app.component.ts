9fd31d0f19b98f379fce10f45cc622ae6f23507c
Cezary <czarun99@gmail.com>
2024-10-05 14:45:25 +0200

    Dodanie nagłówka, footera oraz komponentów browse i register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from './footer/footer.component';
+import {NavbarComponent} from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'movie-merge-frontend2';
+  title = 'movie-merge-frontend';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {BrowseComponent} from './browse/browse.component';
+import {RegisterComponent} from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'browse', component: BrowseComponent},
+];
