6d3bb2d76e0f324c7a0d01be396dbd41c6d7bee5
Cezary <czarun99@gmail.com>
2024-10-08 00:06:31 +0200

    Przechodzenie po kolejnych filmach dodane

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -2,26 +2,33 @@ import {Component, OnInit} from '@angular/core';
 import {Movie, MOVIES} from '../movies';
 import {FormsModule} from '@angular/forms';
 import {NgForOf} from '@angular/common';
+import {MovieService} from '../services/movie.service';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
   imports: [
     FormsModule,
-    NgForOf
+    NgForOf,
+    RouterLink
   ],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.css'
 })
 export class BrowseComponent implements OnInit {
-  movies: Movie[]= MOVIES;
-  filteredMovies: Movie[]= MOVIES;
+  movies: Movie[] = [];
+  filteredMovies: Movie[] = [];
   genres: string[] = ['All', 'Action', 'Sci-Fi', 'Drama', 'Comedy'];
   selectedGenre: string = 'All';
 
-  constructor() {}
+  constructor(private movieService: MovieService) {}
 
   ngOnInit(): void {
+    this.movieService.getMovies().subscribe(movies => {
+      this.movies = movies;
+      this.filteredMovies = movies;
+    });
   }
 
   filterMovies(){
