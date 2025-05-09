92e79e90af7d28f9e15633205e06a74674012e18
Cezary <czarun99@gmail.com>
2025-05-09 14:41:41 +0200

    dodanie nowego pustego komponentu

---
+++ src/app/components/movie/movie-details/movie-details-reviews/movie-details-reviews.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-details-reviews',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-details-reviews.component.html',
+  styleUrl: './movie-details-reviews.component.css'
+})
+export class MovieDetailsReviewsComponent {
+
+}
